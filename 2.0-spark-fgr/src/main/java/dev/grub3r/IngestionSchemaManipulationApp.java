8d1d6986f34f4ecd8b736df736f5d407744c6030
Flo Gruber <49080575+flogruber@users.noreply.github.com>
2021-10-09 11:01:01 +0200

    :construction: Ingestion

--- 2.0-spark-fgr/src/main/java/dev/grub3r/IngestionSchemaManipulationApp.java
+++ 2.0-spark-fgr/src/main/java/dev/grub3r/IngestionSchemaManipulationApp.java
@@ -9,6 +9,8 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import org.apache.spark.sql.functions;
+
 import java.util.Arrays;
 
 public class IngestionSchemaManipulationApp {
@@ -23,6 +25,33 @@ public class IngestionSchemaManipulationApp {
 
         df.show(5);
 
+        df = df.withColumn("county", functions.lit("Wake"))
+            .withColumnRenamed("HSISID", "datasetId")
+            .withColumnRenamed("NAME", "name")
+            .withColumnRenamed("ADDRESS1", "address1")
+            .withColumnRenamed("ADDRESS2", "address2")
+            .withColumnRenamed("CITY", "city")
+            .withColumnRenamed("STATE", "state")
+            .withColumnRenamed("POSTALCODE", "zip")
+            .withColumnRenamed("PHONENUMBER", "tel")
+            .withColumnRenamed("RESTAURANTOPENDATE", "dateStart")
+            .withColumnRenamed("FACILITYTYPE", "type")
+            .withColumnRenamed("X", "geoX")
+            .withColumnRenamed("Y", "geoY")
+            .drop("OBJECTID")
+            .drop("PERMITID")
+            .drop("GEOCODESTATUS");
+
+        df.show(5);
+
+        df = df.withColumn("id", functions.concat(
+            df.col("state"),
+            functions.lit("_"),
+            df.col("county"), functions.lit("_"),
+            df.col("datasetId")
+        ));
+
+        df.show(5);
     }
 
     public static SparkSession setupSparkSession(){
